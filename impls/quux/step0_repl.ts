c4b5d9ffc11c50b68fc538923c394e30862c3c08
qle <ql.le@hotmail.com>
2020-04-28 23:27:00 +0200

    Step 0

---
+++ impls/quux/step0_repl.ts
@@ -0,0 +1,42 @@
+import { createInterface } from 'readline';
+import { flowRight } from 'lodash/fp';
+
+function READ(args: string) {
+    return args;
+}
+
+function EVAL(args: string) {
+    return args;
+}
+
+function PRINT(args: string) {
+    console.log(args);
+}
+
+function rep(args: string) {
+    flowRight(
+        PRINT,
+        EVAL,
+        READ
+    )(args);
+}
+
+function mainLoop() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.setPrompt('user> ');
+    rl.on('line', answer => {
+        if (!answer) {
+            rl.close();
+        } else {
+            rep(answer);
+            rl.prompt();
+        }
+    });
+    rl.prompt();
+}
+
+mainLoop();
